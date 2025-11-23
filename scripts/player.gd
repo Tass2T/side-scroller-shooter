2ef48a42058412165a4cd545970beb1c97040293
Bruno Griveau <brunog90@gmail.com>
2025-11-23 17:03:25 +0100

    Squashed commit of the following:
    commit 424ecc7aa6186da6a5e09cc8a1be2973bdf74706
    Author: Bruno Griveau <brunog90@gmail.com>
    Date:   Sun Nov 23 17:02:14 2025 +0100
    
        playerposition tweaks
    
    commit e5203a71bed773e8f692261a710268ffd3258e98
    Author: Bruno Griveau <brunog90@gmail.com>
    Date:   Sun Nov 23 16:43:13 2025 +0100
    
        keep player from leaving viewport

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,29 @@
 class_name Player extends CharacterBody2D
 
 @onready var bullet_manager: BulletManager = $BulletManager
+@onready var color_rect: ColorRect = $ColorRect
 
-func _physics_process(delta: float) -> void:
-	position = get_viewport().get_mouse_position()
+
+func _physics_process(_delta: float) -> void:
+	
+	manage_position()
 	
 	if Input.is_action_pressed("shoot"):
 		bullet_manager.shoot()
+
+
+func manage_position() -> void:
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
+	var window_size: Vector2 = get_viewport_rect().size
+
+	if mouse_position.y > window_size.y:
+		mouse_position.y = window_size.y 
+	elif mouse_position.y < 0:
+		mouse_position.y = 0 
+		
+	if mouse_position.x - color_rect.size.x / 2 < 0:
+		mouse_position.x = 0 + color_rect.size.x / 2
+	elif mouse_position.x + color_rect.size.x / 2 > window_size.x:
+		mouse_position.x = window_size.x - color_rect.size.x / 2
+		
+	position = mouse_position
