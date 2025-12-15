e623dbc41cd23bc5dd0c5fd0fae327ae47edc42c
Bruno Griveau <brunog90@gmail.com>
2025-12-15 16:34:39 +0100

    debuffed bullets + it's gone after touching something

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,7 +2,7 @@ class_name Bullet extends Area2D
 
 var speed: int = 100
 var bullet_direction: Vector2
-const DAMAGES = 50
+const DAMAGES = 1
 var origin_entity: Node2D
 
 func set_target(target: Vector2) -> void:
@@ -20,6 +20,7 @@ func _on_body_entered(body: Node2D) -> void:
 		return
 		
 	if origin_entity is Player && body is Enemy:
-		body.get_damaged(50)
+		body.get_damaged(DAMAGES)
 	elif origin_entity is Enemy && body is Player:
 		body.is_killed()
+	queue_free()
